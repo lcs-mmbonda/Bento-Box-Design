b9421c08cf8a87850d07be0f568cc471adbe75f5
Myers Mbonda <mmbonda@lcs.on.ca>
2023-11-04 09:47:49 -0400

    I have now completed just over half the tiles in the bento box design

--- srtgbsdx/srtgbsdx/ContentView.swift
+++ srtgbsdx/srtgbsdx/ContentView.swift
@@ -8,49 +8,148 @@
 import SwiftUI
 
 struct ContentView: View {
+    let lightGray = Color(
+            hue: 0/360.0,
+            saturation: 0.0,
+            brightness: 0.93
+        )
+        let mediumGray = Color(
+            hue: 0/360.0,
+            saturation: 0.0,
+            brightness: 0.8
+        )
+        let darkGray = Color(
+            hue: 0/360.0,
+            saturation: 0.0,
+            brightness: 0.45
+        )
+        let lightPurple = Color(
+            hue: 266.0/360.0,
+            saturation: 0.0,
+            brightness: 1.0
+        )
+        let mediumPurple = Color(
+            hue: 266.0/360.0,
+            saturation: 0.35,
+            brightness: 0.50
+        )
+        let darkPurple = Color(
+            hue: 286.0/360.0,
+            saturation: 0.65,
+            brightness: 0.50
+        )
     var body: some View {
         HStack() {
             VStack {
                 HStack {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(Color.gray)
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(Color.gray)
-                }
-                RoundedRectangle(cornerRadius: 25.0)
-                    .fill(Color.gray)
-                HStack {
-                    RoundedRectangle(cornerRadius: 25.0).fill(Color.gray)
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20.0)
+                            .fill(mediumGray)
+                        .frame(width:110, height: 85)
+                        VStack {
+                            Image("thunderbolt")
+                                .resizable()
+                                .scaledToFit()
+                            .frame(width: 50, height: 50)
+
+                            Text("Thunderbolt 4")
+                                .font(.system(size: 10))
+                        }
                         
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(Color.gray)
+                    }
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20.0)
+                            .fill(mediumGray)
+                        .frame(width:110, height: 85)
+                        VStack {
+                            Text("Up to")
+                            Text("20%")
+                                .bold()
+                                .font(.system(size:30))
+                                .foregroundStyle(mediumPurple)
+                                
+                            Text("Faster CPU")
+                        }
+                    }
                 }
-                RoundedRectangle(cornerRadius: 25.0)
-                    .fill(Color.gray)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20.0)
+                        .fill(mediumGray)
+                    .frame(width: 220, height: 150)
+                    VStack {
+                        Text("Over")
+                            .foregroundStyle(mediumPurple)
+                        Text("67 billion")
+                            .bold()
+                            .foregroundStyle(mediumPurple)
+                            .font(.system(size: 40))
+                        Text("transistors")
+                            .foregroundStyle(.purple)
+                            
+                        
+                    }
+                }
+                HStack {
+                    RoundedRectangle(cornerRadius: 20.0)
+                        .fill(mediumGray)
+                        .frame(width:100, height: 80)
+                        
+                    RoundedRectangle(cornerRadius: 20.0)
+                        .fill(mediumGray)
+                        .frame(width:100, height: 80)
+                }
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20.0)
+                        .fill(mediumGray)
+                        .frame(width:230, height: 70)
+                    VStack {
+                        Text("High-performance media engine").bold()
+                        Text("with ProRes").bold()
+                    }
+                }
             }
             VStack {
                 HStack {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(Color.gray)
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(Color.gray)
-                        .frame(width:115)
+                    ZStack {
                         
+                            RoundedRectangle(cornerRadius: 20.0)
+                                .fill(mediumGray)
+                            .frame(width:100, height: 80)
+                        VStack {
+                            Text("Up to")
+                            Text("30%")
+                                .bold()
+                                .font(.system(size:30))
+                                .foregroundStyle(darkPurple)
+                            Text("Faster GPU")
+                        }
+                    }
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20.0)
+                            .fill(mediumGray)
+                        .frame(width:180, height: 80)
+                        
+                        Text("    Industry leading performance per watt")
+                            .bold()
+                            .font(.system(size:15))
+                            .foregroundStyle(darkPurple)
+                            
+                    }
                 }
                 ZStack {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(Color.gray)
-                        .frame(width: 210, height: 200)
+                    RoundedRectangle(cornerRadius: 20.0)
+                        .fill(mediumGray)
+                        .frame(width: 300, height: 225)
                         
                     Rectangle()
                         .fill(Color.black)
-                        .frame(width: 140, height: 130)
+                        .frame(width: 170, height: 170)
                     .frame(height: 200)
                     
                     VStack {
                         Text(" M2")
                             .foregroundColor(.white)
-                        .font(.system(size: 30))
+                        .font(.system(size: 40))
                         
                         Text("MAX")
                             .foregroundColor(.purple)
@@ -64,8 +163,9 @@ struct ContentView: View {
                 
                     
                 ZStack {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(Color.gray)
+                    RoundedRectangle(cornerRadius: 20.0)
+                        .fill(mediumGray)
+                        .frame(width:300, height: 70)
                     VStack {
                         Text("Second generation")
                             .font(.system(size:10))
@@ -80,25 +180,63 @@ struct ContentView: View {
                 }
             }
             VStack {
-                RoundedRectangle(cornerRadius: 25.0)
-                    .fill(Color.gray)
-                HStack {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(Color.gray)
-                        .frame(height:160)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20.0)
+                        .fill(mediumGray)
+                    .frame(width: 200, height: 85)
+                    VStack {
+                        Text("Up to")
                         
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(Color.gray)
-                        .frame(height:160)
-                        
+                        Text("96G")
+                            .bold()
+                            .font(.system(size:40))
+                            .foregroundColor(.purple
+                            )
+                        Text("LPDDR5 memory")
+                    }
                 }
-                RoundedRectangle(cornerRadius: 25.0)
-                    .fill(Color.gray)
-                    .frame(height:115)
+                
+                    
+                
+                        HStack {
+                                VStack {
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 20.0)
+                                            .fill(mediumGray)
+                                        .frame(height:160)
+                                        VStack {
+                                            Image("Cpu")
+                                                .resizable()
+                                                .scaledToFit()
+                                            .frame(width: 100, height: 120)
+                                            Text("12-core")
+                                                .font(.system(size: 18))
+                                                .foregroundStyle(mediumPurple)
+                                            Text("CPU")
+                                                .font(.system(size:12))
+                                        }
+                                    }
+                                  
+                                    
+                                
+                            }
+                            
+                                
+                            RoundedRectangle(cornerRadius: 20.0)
+                                .fill(mediumGray)
+                                .frame(height:160)
+                                
+                        
+                    
+                }
+                RoundedRectangle(cornerRadius: 20.0)
+                    .fill(mediumGray)
+                    .frame(height:130)
                 
             }
         }
-    
+        .padding()
+        .frame( width: 800, height: 430)
     
     }
         

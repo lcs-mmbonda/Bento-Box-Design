a800df1eaa7f599258cc2f79f0e37f6239539f46
Myers Mbonda <mmbonda@lcs.on.ca>
2023-11-08 08:51:18 -0500

    Made the selected tab item default to the Stopwatch.

--- StopwatchApp/StopwatchApp/ContentView.swift
+++ StopwatchApp/StopwatchApp/ContentView.swift
@@ -25,7 +25,9 @@ struct ContentView: View {
                     Spacer()
                     
                     CircleButton(buttonColor: Color("Dark Green") , label: "Start", labelColor: .green)
+                    
                 }
+                
                 
                 
             }
@@ -34,5 +36,41 @@ struct ContentView: View {
     }
 }
 #Preview {
-    ContentView()
+    TabView(selection: Binding.constant(3)) {
+        
+        Text("World Clock")
+            .tabItem {
+                Image(systemName: "globe")
+                Text ("World Clock")
+            }
+            .tag(1)
+        Text("Alarm" )
+            .tabItem{
+                Image (systemName: "alarm")
+                Text ("Alarm")
+            }
+            .tag(2)
+        
+        ContentView()
+            .tabItem {
+                Image(systemName: "stopwatch")
+                Text ("Stopwatch")
+            }
+            .tag(3)
+        
+        Text("Timer")
+            .tabItem {
+                Image (systemName:"timer")
+                Text ("Timer")
+                
+            }
+            .tag(4)
+            
+            
+            
+      }
+      .accentColor(.orange)
+      
 }
+
+
--- StopwatchApp/StopwatchApp/StopwatchAppApp.swift
+++ StopwatchApp/StopwatchApp/StopwatchAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct StopwatchAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            
         }
     }
 }
